b019d25eedbe82ff153c652969af3dc677f23783
Dave <daveg7lee@gmail.com>
2022-01-05 18:56:41 +0900

    Make ToDo List Header

---
+++ styles/palette.ts
@@ -0,0 +1,11 @@
+export default {
+  red: '#FFAFBO',
+  orange: '#FFC282',
+  yellow: '#FCFFBO',
+  green: '#E2FFAF',
+  blue: '#AEE4FF',
+  navy: '#B5C7ED',
+  gray: '#E5E5E5',
+  deep_red: '#F35456',
+  deep_green: '#47E774',
+};
